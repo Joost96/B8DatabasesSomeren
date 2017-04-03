0588fae7d50c1931f5fbb691a6f0fe31aec9065d
Joost96 <joost-k@live.nl>
2017-04-03 12:45:55 +0200

    docent menu

--- Someren/Someren_Form.cs
+++ Someren/Someren_Form.cs
@@ -140,5 +140,12 @@ namespace Someren
         {
 
         }
+
+        private void toonDocentenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.panel1.Controls.Clear();
+            this.groupBox1.Text = "Docenten";
+            this.panel1.Controls.Add(SomerenUI.showDocenten());
+        }
     }
 }
